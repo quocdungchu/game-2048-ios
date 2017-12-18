b00ed0420439265dfb74cabf25d4bc67cad99b05
Quoc Dung Chu <quocdung.chu@gmail.com>
2017-12-18 17:15:01 +0100

    Improve

--- game2048-ios/BoardLogic.swift
+++ game2048-ios/BoardLogic.swift
@@ -2,60 +2,72 @@ import RxSwift
 import Foundation
 
 enum BoardEvent {
-    case didTilesReset
-    case didTileChange(from: Board.Tile, to: Board.Tile, at: Board.Position)
+    case didTilesReset(board: Board)
+    case didTileGenerate(board: Board, point: Int)
+    case didTileMove(board: Board, from: Board.Position, to: Board.Position)
+    case didTileTransform(board: Board, from: Board.Position, to: Board.Position, newPoint: Int)
 }
 
 protocol BoardLogic {
-    func fillRandomTile(board: Board)
-    func moveTiles(to direction: Direction, board: Board)
+    var didEventSend: PublishSubject<BoardEvent> { get }
+    func fillRandomTile()
+    func moveTiles(to direction: Direction)
+    func reset()
 }
 
 class BoardLogicImpl {
     
-    private let didEventSend: PublishSubject<BoardEvent>
+    let didEventSend = PublishSubject<BoardEvent>()
+    private let board: Board
     
-    init(didEventSend: PublishSubject<BoardEvent>) {
-        self.didEventSend = didEventSend
+    init(board: Board) {
+        self.board = board
     }
     
-    private func moveTilesToLeft(board: Board) {
+    private func moveTilesToLeft() {
         
     }
     
-    private func moveTilesToRight(board: Board) {
+    private func moveTilesToRight() {
         
     }
     
-    private func moveTilesToUp(board: Board) {
+    private func moveTilesToUp() {
 
     }
     
-    private func moveTilesToDown(board: Board) {
+    private func moveTilesToDown() {
 
     }    
 }
 
 extension BoardLogicImpl: BoardLogic {
-    func fillRandomTile(board: Board) {
+    func fillRandomTile() {
         if let position = board.randomEmptyPosition {
-            board[position] = .filled(point: 2)
+            let point = 2
+            board[position] = .filled(point: point)
+            didEventSend.onNext(.didTileGenerate(board: board, point: point))
         }
     }
     
-    func moveTiles(to direction: Direction, board: Board) {
+    func moveTiles(to direction: Direction) {
         switch direction {
         case .down:
-            moveTilesToDown(board: board)
+            moveTilesToDown()
 
         case .up:
-            moveTilesToUp(board: board)
+            moveTilesToUp()
 
         case .left:
-            moveTilesToLeft(board: board)
+            moveTilesToLeft()
             
         case .right:
-            moveTilesToRight(board: board)
+            moveTilesToRight()
         }
+    }
+    
+    func reset() {
+        board.reset()
+        didEventSend.onNext(.didTilesReset(board: board))
     }
 }
--- game2048-ios/GameState.swift
+++
@@ -1,7 +0,0 @@
-class GameState {
-    var score: Int = 0
-    
-    func reset() {
-        self.score = 0
-    }
-}
